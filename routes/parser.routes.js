619f865bf23a410391ea693a83bf57b8b0d55852
Dhiwakar <iamdhiwakar@gmail.com>
2025-10-19 12:34:59 +0530

    endpoint method change

--- routes/parser.routes.js
+++ routes/parser.routes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const parserController = require("../controller/parser.controller");
 
-router.get("/", parserController.parser);
-router.get("/gs1", parserController.GS1_parser);
-router.get("/hibc", parserController.HIBC_parser);
-router.get("/ean_13", parserController.EAN_13_parser);
+router.post("/", parserController.parser);
+router.post("/gs1", parserController.GS1_parser);
+router.post("/hibc", parserController.HIBC_parser);
+router.post("/ean_13", parserController.EAN_13_parser);
 
 module.exports = router;
