f4939f31f87c7b003aac8b8df5309e4471d93987
SANKALP PANDEY <tigerishan426@gmail.com>
2025-10-19 14:05:07 +0530

    Feature: started working on the auth implementation, key generation and rate limiter for the user

---
+++ controller/user.controller.js
@@ -0,0 +1,9 @@
+module.exports = {
+  createUser: async (req, res) => {},
+  logInUser: async (req, res) => {},
+  getApiLimitAvaible: async (req, res) => {},
+  getKeyAssigned: async (req, res) => {},
+  extendFreeLimit: async (req, res) => {},
+  managekey: async (req, res) => {},
+  deleteKey: async (req, res) => {},
+};
